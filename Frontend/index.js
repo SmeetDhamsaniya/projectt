cf51a0eb96adf513443942afdb21a6fe9b900242
SmeetDhamsaniya <smit.dhamsaniya2603@gmail.com>
2024-10-09 21:35:01 +0530

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom'; // Changed from 'react-dom/client' to 'react-dom'
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Using ReactDOM.render instead of ReactDOM.createRoot
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Targeting the root element in your HTML
+);
+
+// If you want to measure performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render();
